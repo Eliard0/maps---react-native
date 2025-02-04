5ce392ea0ec032cd0e186c0d16887c3f8b9b005a
Eliardo <eliardosilva81@gmail.com>
2025-02-04 09:03:54 -0300

    estilizando o mapa

--- App.js
+++ App.js
@@ -1,28 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { useState } from 'react';
 import {
-  Alert,
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
+  Button, StyleSheet, Text, TouchableOpacity, View,
 } from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 function App() {
   const [region, setRegion] = useState({
     latitude: 51.5085300,
     longitude: -0.1257400,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01
   })
 
   function moveCidade(lat, log) {
@@ -36,13 +24,32 @@ function App() {
 
   return (
     <View style={styles.sectionContainer}>
-      <Text>ola mundo sim</Text>
-      <Button title='mudando para frança' onPress={() => moveCidade(48.8566, 2.3522)} />
-      <Button title='mudando para japão' onPress={() => moveCidade(35.682839, 139.759455)} />
       <MapView
-        style={{ height: 350, width: 350 }}
+        // mapType='standard' | satellit | hybrid
+        style={{ height: 450, width: '100%' }}
         region={region}
-      />
+        showsTraffic={true}
+      >
+        <Marker image={require('./src/images/carro.png')} coordinate={{
+          latitude: 51.5085300,
+          longitude: -0.1257400,
+        }}>
+          <Text>ola mundo sim</Text>
+          <View style={{ width: 150, backgroundColor: 'red', height: 150, opacity: 0.2 }}></View>
+        </Marker>
+
+      </MapView>
+      <View>
+        <Text style={styles.title}>Selecione um botão abaixo para mudar a localização</Text>
+        <View style={styles.viewButtons}>
+          <TouchableOpacity style={styles.moveCityButton} onPress={() => moveCidade(48.8566, 2.3522)} >
+            <Text style={styles.textButtonMoveCity}>Mudando para frança</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.moveCityButton} onPress={() => moveCidade(35.682839, 139.759455)}>
+            <Text style={styles.textButtonMoveCity}>Mudando para japão</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -50,21 +57,35 @@ function App() {
 const styles = StyleSheet.create({
   sectionContainer: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  title: {
+    marginTop: 10,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#000',
+    textAlign: 'center'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  viewButtons: {
+    flexDirection: 'row'
   },
-  highlight: {
-    fontWeight: '700',
+
+  moveCityButton: {
+    backgroundColor: '#228FE9',
+    width: '45%',
+    height: 45,
+    marginHorizontal: 10,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 15
   },
+   textButtonMoveCity: {
+    color: '#fff', 
+    fontWeight: 'bold'
+   }
 });
 
 export default App;
